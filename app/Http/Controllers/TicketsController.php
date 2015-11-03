1f42e713701bbcbcb6606ef0358cbe2005438b79
Yoel Monzon <yoelfme@hotmail.com>
2015-11-03 01:17:40 -0600

    [add] views and update gulpfile.js

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -10,27 +10,27 @@ class TicketsController extends Controller {
 
 	public function latest()
     {
-        dd('latest');
+        return view('tickets.list');
     }
 
     public function popular()
     {
-        dd('popular');
+        return view('tickets.list');
     }
 
     public function open()
     {
-        dd('open');
+        return view('tickets.list');
     }
 
     public function closed()
     {
-        dd('closed');
+        return view('tickets.list');
     }
 
     public function detail($id)
     {
-        dd('detail: ' . $id);
+        return view('tickets.details');
     }
 
 }
