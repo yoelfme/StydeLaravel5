7f06ff16d171be02675887a6420c5a00a962147f
Yoel Monzon <yoelfme@hotmail.com>
2015-12-07 23:53:49 -0600

    [add] relations to models

---
+++ app/Entities/Entity.php
@@ -0,0 +1,13 @@
+<?php
+namespace TeachMe\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entity extends Model
+{
+    
+    public static function getClass()
+    {
+        return get_class(new static);
+    }
+}
