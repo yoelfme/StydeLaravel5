96f98a1155c366c941553ca6d50f4994a84e8a4c
Yoel Monzon <yoelfme@hotmail.com>
2015-12-09 05:55:30 +0000

    [add] language files for spanish

---
+++ resources/lang/es/tickets.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'latest_title' => 'Solicitudes recientes',
+    'open_title' => 'Solicitudes abiertas',
+    'closed_title' => 'Tutoriales',
+    'popular_title' => 'Solicitudes populares',
+    'latest_total' => '{0} No hay solicitudes recientes'
+                    .'|{1} Solo hay una solicitud reciente'
+                    .'|[2,Inf] Hay :count solicitudes recientes',
+    'open_total' => '{0} No hay solicitudes abiertas'
+                    .'|{1} Solo hay una solicitud abierta'
+                    .'|[2,Inf] Hay :count solicitudes abiertas',
+    'closed_total' => '{0} No hay tutoriales'
+                    .'|{1} Solo hay un tutorial'
+                    .'|[2,Inf] Hay :count tutoriales',
+    'popular_total' => '{0} No hay solicitudes populares'
+                    .'|{1} Solo hay una solicitud popular'
+                    .'|[2,Inf] Hay :count solicitudes populares',
+    'status' => [
+        'open' => 'Abierta',
+        'closed' => 'Finalizada'
+    ]
+];
