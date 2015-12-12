752a1b766b20b41b5487d61ce50bba62d8fad8ec
Yoel Monzon <yoelfme@hotmail.com>
2015-12-12 03:28:29 +0000

    [add] view for create a ticket

---
+++ resources/views/tickets/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h2>Nueva Solicitud</h2>
+            {!! Form::open(['route' => 'tickets.store', 'method' => 'POST']) !!}
+                <p>
+                    <button type="submit" class="btn btn-primary">Enviar Solicitud</button>
+                </p>
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
+@endsection
