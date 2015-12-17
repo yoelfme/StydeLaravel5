ca16bdd9973cb4bc0497a40d0f31b53320265d08
Yoel Monzon <yoelfme@hotmail.com>
2015-12-16 18:03:35 -0600

    [update] eager loading for paginate tickets

--- resources/views/tickets/partials/item.blade.php
+++ resources/views/tickets/partials/item.blade.php
@@ -12,12 +12,13 @@
             <span class="glyphicon glyphicon-thumbs-down"></span> No votar
         </a> --}}
 
-        <a href="{{ route('tickets.detail', $ticket) }}">
+        {{-- <a href="{{ route('tickets.detail', $ticket) }}">
             <span class="votes-count">{{ $ticket->voters()->count() }} votos</span>
             - <span class="comments-count">{{ $ticket->comments()->count() }} comentarios</span>.
-        </a>
+        </a> --}}
     <p>
     <p class="date-t">
         <span class="glyphicon glyphicon-time"></span> {{ $ticket->created_at->format('d/m/Y h:ia')}}
+        por {{ $ticket->author->name }}
     </p>
 </div>
