7cdc78d882d3a7c3f666c8fd93703385d162a1e0
Yoel Monzon <yoelfme@hotmail.com>
2015-12-23 23:49:38 -0600

    [add] function to unvote ticket

--- public/js/app.js
+++ public/js/app.js
@@ -34,4 +34,38 @@ $(document).ready(function () {
             alert.error('Ocurrio un error!');
         });
     });
+
+    $('.btn-unvote').click(function (e) {
+        e.preventDefault();
+
+        var form = $('#form-unvote');
+
+        var button = $(this);
+        var ticket = button.closest('.ticket');
+        var id = ticket.data('id');
+
+        var action = form.prop('action').replace(':id', id);
+
+        button.addClass('hide');
+
+        $.post(action, form.serialize(), function (response) {
+            // alert
+            // update count votes
+            ticket.find('.btn-vote').removeClass('hide');
+
+            alert.success('Voto eliminado!');
+
+            var voteCount = ticket.find('.votes-count');
+            var votes = parseInt(voteCount.text().split(' ')[0]);
+            votes--;
+
+            voteCount.text(votes <= 1 ? votes + ' voto' : votes + ' votos');
+        }).fail(function () {
+            // print error message
+            button.removeClass('hide');
+
+            alert.error('Ocurrio un error!');
+        });
+    });
+
 });
