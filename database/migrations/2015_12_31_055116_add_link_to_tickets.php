e2e85f4d298ec1e2fef448abd36626dc99c216cb
Yoel Monzon <yoelfme@hotmail.com>
2015-12-30 23:53:09 -0600

    [add] migration to add field  to tickets model

---
+++ database/migrations/2015_12_31_055116_add_link_to_tickets.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLinkToTickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->string('link')->after('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropColumn('link');
+        });
+    }
+}
